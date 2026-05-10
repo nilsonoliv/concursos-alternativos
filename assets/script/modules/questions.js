9b1705634ab85391d034d0bc23ae1e3e69fb2fcb
Reinilson Oliveira <nilsonespiritosant@gmail.com>
2026-05-10 16:06:49 -0300

    Refactor question imports: replace individual question packs with consolidated packs (pack1, pack2, pack3)

--- assets/script/modules/questions.js
+++ assets/script/modules/questions.js
@@ -1,19 +1,17 @@
 
-import { conhBasicos } from './modules-questions/conh-basicos.js';
-import { conhComple } from './modules-questions/conh-comple.js';
-import { conhEspec } from './modules-questions/conh-espec.js';
+import { pack1 } from './modules-questions/pack1.js';
+import { pack2 } from './modules-questions/pack2.js';
+import { pack3 } from './modules-questions/pack3.js';
 
 /* ==========================================================================
    BLOCO 1: DADOS DE ORIGEM (MOCK DATA)
    ========================================================================== */
  export const QUESTOES_PADRAO = [
    
-...conhBasicos,
-...conhComple,
-...conhEspec,
-
+...pack1,
+...pack2,
+...pack3,
 
 ];
-
 
 
