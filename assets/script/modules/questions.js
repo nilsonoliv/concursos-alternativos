83015bca26533ddc91d6f430988998aa95c52d4b
Reinilson Oliveira <nilsonespiritosant@gmail.com>
2026-06-01 15:30:46 -0300

    Add pack4 to questions module imports and data aggregation

--- assets/script/modules/questions.js
+++ assets/script/modules/questions.js
@@ -2,6 +2,7 @@
 import { pack1 } from './modules-questions/pack1.js';
 import { pack2 } from './modules-questions/pack2.js';
 import { pack3 } from './modules-questions/pack3.js';
+import { pack4 } from './modules-questions/pack4.js';
 
 /* ==========================================================================
    BLOCO 1: DADOS DE ORIGEM (MOCK DATA)
@@ -11,6 +12,7 @@ import { pack3 } from './modules-questions/pack3.js';
 ...pack1,
 ...pack2,
 ...pack3,
+...pack4,
 ];
 
 
